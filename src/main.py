c3c7133d5195aa1e556e6b87159249b40515a50e
nagarajarchak <archak.nagaraj@gmail.com>
2024-08-03 22:41:00 +1000

    Added html, css dependencies

--- src/main.py
+++ src/main.py
@@ -1,21 +1,14 @@
 import logging
-from flask import Flask, redirect, request, render_template, session, url_for
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 logging.basicConfig(level = logging.INFO)
 log = logging.getLogger(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods = ['GET'])
 def index():
     """
     A function rendering landing page.
     """
 
-    if request.method == 'GET':
-        try:
-            # Render landing page
-            return render_template('index.html', error = False)
-        except Exception as e:
-            log.error(f"Exception: {e}")
-    
-    return render_template('login.html',error = False)
+    return render_template('index.html', error = False)
