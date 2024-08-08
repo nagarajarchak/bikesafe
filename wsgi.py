d1d13c1cd031abbfb9d4f01b0bad026b9283aaa3
nagarajarchak <archak.nagaraj@gmail.com>
2024-08-09 00:44:23 +1000

    Added DB shutdown in Heroku entrypoint script

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,8 @@
-from app.main import app
+from app.main import app, pg_conn, pg_cursor
 
 if __name__ == '__main__':
-    app.run()
+    try: 
+        app.run()
+    finally: 
+        pg_conn.close()
+        pg_cursor.close()
